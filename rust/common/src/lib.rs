f284180871c1759ebd1763f575c9f99e89e78612
Gopa Kumar <gopakumarce@gmail.com>
2021-02-25 18:46:43 -0500

    modified transport APIs to reflect async non blocking behaviour

--- rust/common/src/lib.rs
+++ rust/common/src/lib.rs
@@ -10,9 +10,9 @@ pub struct NxtHdr {}
 
 pub trait Transport {
     fn Dial(&mut self);
-    fn NewStream(&mut self) -> Box<dyn Transport>;
-    fn Close(&mut self) -> Result<(), NxtError>;
-    fn IsClosed(&self) -> bool;
-    fn Read(&mut self) -> Result<(NxtHdr, Vec<Vec<u8>>), NxtError>;
-    fn Write(&mut self, hdr: &NxtHdr) -> Result<(), NxtError>;
+    fn NewStream(&mut self) -> u64;
+    fn Close(&mut self, stream: u64) -> Result<(), NxtError>;
+    fn IsClosed(&self, stream: u64) -> bool;
+    fn Read(&mut self) -> Result<(u64, NxtHdr, Vec<Vec<u8>>), NxtError>;
+    fn Write(&mut self, stream: u64, hdr: &NxtHdr) -> Result<(), NxtError>;
 }
--- rust/transport/websocket/src/lib.rs
+++ rust/transport/websocket/src/lib.rs
@@ -9,50 +9,67 @@ struct WebSession {
     serverName: String,
     nextStream: AtomicU64,
     caCert: Vec<u8>,
-    streams: HashMap<u64, Arc<WebStream>>,
+    streams: HashMap<u64, WebStream>,
+    server: bool,
 }
 struct WebStream {
-    server: bool,
-    stream: u64,
     closed: bool,
-    session: Arc<Mutex<WebSession>>,
 }
 
-impl common::Transport for WebStream {
+impl WebStream {
+    fn NewClient(
+        caCert: Vec<u8>,
+        servername: &str,
+        port: usize,
+    ) -> (Box<dyn common::Transport>, u64) {
+        let stream = WebStream { closed: false };
+        let mut streams = HashMap::new();
+        // First stream with streamid 0
+        streams.insert(0, stream);
+        (
+            Box::new(WebSession {
+                port,
+                serverName: servername.to_string(),
+                caCert,
+                streams,
+                nextStream: AtomicU64::new(0),
+                server: false,
+            }),
+            0,
+        )
+    }
+}
+
+impl common::Transport for WebSession {
     fn Dial(&mut self) {}
 
-    fn NewStream(&mut self) -> Box<dyn Transport> {
-        let s = self.session.lock().unwrap();
+    fn NewStream(&mut self) -> u64 {
         let sid;
         // server has odd number streamids and client is even, just to prevent overlap
         if self.server {
-            sid = 2 * s.nextStream.fetch_add(1, Ordering::Relaxed) + 1;
+            sid = 2 * self.nextStream.fetch_add(1, Ordering::Relaxed) + 1;
         } else {
-            sid = 2 * s.nextStream.fetch_add(1, Ordering::Relaxed);
+            sid = 2 * self.nextStream.fetch_add(1, Ordering::Relaxed);
         }
-        drop(s);
-        Box::new(WebStream {
-            server: false,
-            stream: sid,
-            closed: false,
-            session: self.session.clone(),
-        })
+        let stream = WebStream { closed: false };
+        self.streams.insert(sid, stream);
+        sid
     }
 
-    fn Close(&mut self) -> Result<(), common::NxtError> {
+    fn Close(&mut self, stream: u64) -> Result<(), common::NxtError> {
         Ok(())
     }
 
-    fn IsClosed(&self) -> bool {
-        self.closed
+    fn IsClosed(&self, stream: u64) -> bool {
+        false
     }
 
-    fn Read(&mut self) -> Result<(common::NxtHdr, Vec<Vec<u8>>), common::NxtError> {
+    fn Read(&mut self) -> Result<(u64, common::NxtHdr, Vec<Vec<u8>>), common::NxtError> {
         Err(common::NxtError {
             code: common::NxtErr::GENERAL_ERR,
         })
     }
-    fn Write(&mut self, hdr: &common::NxtHdr) -> Result<(), common::NxtError> {
+    fn Write(&mut self, stream: u64, hdr: &common::NxtHdr) -> Result<(), common::NxtError> {
         Ok(())
     }
 }
