ed34ad47c4cfa821546af57eae53ab187147eca8
Gopa Kumar <gopakumarce@gmail.com>
2022-02-19 09:22:16 -0500

    expect None stream for netconn

--- rust/transport/netconn/src/lib.rs
+++ rust/transport/netconn/src/lib.rs
@@ -104,173 +104,191 @@ impl common::Transport for NetConn {
     }
 
     fn read(&mut self) -> Result<(u64, NxtBufs), NxtError> {
-        let mut buf;
-        if self.proto == common::TCP {
-            buf = match common::pool_get(self.tcp_pool.clone()) {
-                Some(b) => b,
-                None => {
-                    return Err(NxtError {
-                        code: CONNECTION,
-                        detail: "".to_string(),
-                    });
-                }
-            };
-        } else {
-            buf = match common::pool_get(self.pkt_pool.clone()) {
-                Some(b) => b,
-                None => {
-                    return Err(NxtError {
-                        code: CONNECTION,
-                        detail: "".to_string(),
-                    });
-                }
-            };
-        }
-        match self.stream.as_mut().unwrap() {
-            RawStream::Tcp(s) => match s.read(&mut buf[0..]) {
-                Ok(size) => {
-                    // Why would we get read of size 0 ? Even if its a non blocking
-                    // socket, that should be an error WouldBlock
-                    if size == 0 {
+        if let Some(s) = self.stream.as_mut() {
+            let mut buf;
+            if self.proto == common::TCP {
+                buf = match common::pool_get(self.tcp_pool.clone()) {
+                    Some(b) => b,
+                    None => {
                         return Err(NxtError {
                             code: CONNECTION,
                             detail: "".to_string(),
                         });
                     }
-                    unsafe { buf.set_len(size) }
-                    Ok((
-                        0,
-                        NxtBufs {
-                            hdr: None,
-                            bufs: vec![buf],
-                            headroom: 0,
-                        },
-                    ))
-                }
-                Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => Err(NxtError {
-                    code: EWOULDBLOCK,
-                    detail: "".to_string(),
-                }),
-                Err(e) => {
-                    return Err(NxtError {
-                        code: CONNECTION,
-                        detail: format!("{}", e),
-                    })
-                }
-            },
-            RawStream::Udp(s) => match s.recv(&mut buf[0..]) {
-                Ok(size) => {
-                    // Why would we get read of size 0 ? Even if its a non blocking
-                    // socket, that should be an error WouldBlock
-                    if size == 0 {
+                };
+            } else {
+                buf = match common::pool_get(self.pkt_pool.clone()) {
+                    Some(b) => b,
+                    None => {
                         return Err(NxtError {
                             code: CONNECTION,
                             detail: "".to_string(),
                         });
                     }
-                    unsafe { buf.set_len(size) }
-                    Ok((
-                        0,
-                        NxtBufs {
-                            hdr: None,
-                            bufs: vec![buf],
-                            headroom: 0,
-                        },
-                    ))
-                }
-                Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => Err(NxtError {
-                    code: EWOULDBLOCK,
-                    detail: "".to_string(),
-                }),
-                Err(e) => {
-                    return Err(NxtError {
-                        code: CONNECTION,
-                        detail: format!("{}", e),
-                    })
-                }
-            },
-            _ => panic!("Unexpected stream type"),
-        }
-    }
-
-    fn write(&mut self, _: u64, mut data: NxtBufs) -> Result<(), (Option<NxtBufs>, NxtError)> {
-        while !data.bufs.is_empty() {
-            let d = data.bufs.first().unwrap();
-            match self.stream.as_mut().unwrap() {
-                RawStream::Tcp(s) => match s.write(&d[data.headroom..]) {
-                    Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                        return Err((
-                            Some(data),
-                            NxtError {
-                                code: EWOULDBLOCK,
-                                detail: "".to_string(),
-                            },
-                        ));
-                    }
-                    Err(e) => {
-                        return Err((
-                            None,
-                            NxtError {
-                                code: CONNECTION,
-                                detail: format!("{}", e),
-                            },
-                        ));
-                    }
+                };
+            }
+            match s {
+                RawStream::Tcp(s) => match s.read(&mut buf[0..]) {
                     Ok(size) => {
-                        let remaining = d[data.headroom..].len() - size;
-                        if remaining == 0 {
-                            data.bufs.remove(0);
-                            data.headroom = 0;
-                        } else {
-                            data.headroom += size;
-                            return Err((
-                                Some(data),
-                                NxtError {
-                                    code: EWOULDBLOCK,
-                                    detail: "".to_string(),
-                                },
-                            ));
+                        // Why would we get read of size 0 ? Even if its a non blocking
+                        // socket, that should be an error WouldBlock
+                        if size == 0 {
+                            return Err(NxtError {
+                                code: CONNECTION,
+                                detail: "".to_string(),
+                            });
                         }
+                        unsafe { buf.set_len(size) }
+                        Ok((
+                            0,
+                            NxtBufs {
+                                hdr: None,
+                                bufs: vec![buf],
+                                headroom: 0,
+                            },
+                        ))
+                    }
+                    Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => Err(NxtError {
+                        code: EWOULDBLOCK,
+                        detail: "".to_string(),
+                    }),
+                    Err(e) => {
+                        return Err(NxtError {
+                            code: CONNECTION,
+                            detail: format!("{}", e),
+                        })
                     }
                 },
-                RawStream::Udp(s) => match s.send(&d[data.headroom..]) {
-                    Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                        return Err((
-                            Some(data),
-                            NxtError {
-                                code: EWOULDBLOCK,
-                                detail: "".to_string(),
-                            },
-                        ));
-                    }
-                    Err(e) => {
-                        return Err((
-                            None,
-                            NxtError {
-                                code: CONNECTION,
-                                detail: format!("{}", e),
-                            },
-                        ));
-                    }
+                RawStream::Udp(s) => match s.recv(&mut buf[0..]) {
                     Ok(size) => {
-                        let remaining = d[data.headroom..].len() - size;
-                        if remaining == 0 {
-                            data.bufs.remove(0);
-                            data.headroom = 0;
-                        } else {
-                            data.headroom += size;
-                            return Err((
-                                Some(data),
-                                NxtError {
-                                    code: EWOULDBLOCK,
-                                    detail: "".to_string(),
-                                },
-                            ));
+                        // Why would we get read of size 0 ? Even if its a non blocking
+                        // socket, that should be an error WouldBlock
+                        if size == 0 {
+                            return Err(NxtError {
+                                code: CONNECTION,
+                                detail: "".to_string(),
+                            });
                         }
+                        unsafe { buf.set_len(size) }
+                        Ok((
+                            0,
+                            NxtBufs {
+                                hdr: None,
+                                bufs: vec![buf],
+                                headroom: 0,
+                            },
+                        ))
+                    }
+                    Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => Err(NxtError {
+                        code: EWOULDBLOCK,
+                        detail: "".to_string(),
+                    }),
+                    Err(e) => {
+                        return Err(NxtError {
+                            code: CONNECTION,
+                            detail: format!("{}", e),
+                        })
                     }
                 },
                 _ => panic!("Unexpected stream type"),
             }
+        } else {
+            return Err(NxtError {
+                code: EWOULDBLOCK,
+                detail: "stream not ready".to_string(),
+            });
+        }
+    }
+
+    fn write(&mut self, _: u64, mut data: NxtBufs) -> Result<(), (Option<NxtBufs>, NxtError)> {
+        if let Some(s) = self.stream.as_mut() {
+            while !data.bufs.is_empty() {
+                let d = data.bufs.first().unwrap();
+                match s {
+                    RawStream::Tcp(s) => match s.write(&d[data.headroom..]) {
+                        Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                            return Err((
+                                Some(data),
+                                NxtError {
+                                    code: EWOULDBLOCK,
+                                    detail: "".to_string(),
+                                },
+                            ));
+                        }
+                        Err(e) => {
+                            return Err((
+                                None,
+                                NxtError {
+                                    code: CONNECTION,
+                                    detail: format!("{}", e),
+                                },
+                            ));
+                        }
+                        Ok(size) => {
+                            let remaining = d[data.headroom..].len() - size;
+                            if remaining == 0 {
+                                data.bufs.remove(0);
+                                data.headroom = 0;
+                            } else {
+                                data.headroom += size;
+                                return Err((
+                                    Some(data),
+                                    NxtError {
+                                        code: EWOULDBLOCK,
+                                        detail: "".to_string(),
+                                    },
+                                ));
+                            }
+                        }
+                    },
+                    RawStream::Udp(s) => match s.send(&d[data.headroom..]) {
+                        Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                            return Err((
+                                Some(data),
+                                NxtError {
+                                    code: EWOULDBLOCK,
+                                    detail: "".to_string(),
+                                },
+                            ));
+                        }
+                        Err(e) => {
+                            return Err((
+                                None,
+                                NxtError {
+                                    code: CONNECTION,
+                                    detail: format!("{}", e),
+                                },
+                            ));
+                        }
+                        Ok(size) => {
+                            let remaining = d[data.headroom..].len() - size;
+                            if remaining == 0 {
+                                data.bufs.remove(0);
+                                data.headroom = 0;
+                            } else {
+                                data.headroom += size;
+                                return Err((
+                                    Some(data),
+                                    NxtError {
+                                        code: EWOULDBLOCK,
+                                        detail: "".to_string(),
+                                    },
+                                ));
+                            }
+                        }
+                    },
+                    _ => panic!("Unexpected stream type"),
+                }
+            }
+        } else {
+            // No one should try writing before a dial() is called
+            return Err((
+                None,
+                NxtError {
+                    code: CONNECTION,
+                    detail: "stream not ready".to_string(),
+                },
+            ));
         }
         Ok(())
     }
@@ -281,38 +299,45 @@ impl common::Transport for NetConn {
         poll: &mut Poll,
         register: RegType,
     ) -> Result<(), NxtError> {
-        match register {
-            RegType::Reg => match self.stream.as_mut().unwrap() {
-                RawStream::Tcp(stream) => poll.registry().register(
-                    stream,
-                    token,
-                    Interest::READABLE | Interest::WRITABLE,
-                )?,
-                RawStream::Udp(stream) => poll.registry().register(
-                    stream,
-                    token,
-                    Interest::READABLE | Interest::WRITABLE,
-                )?,
-                _ => panic!("Unexpected stream type"),
-            },
-            RegType::Dereg => match self.stream.as_mut().unwrap() {
-                RawStream::Tcp(stream) => poll.registry().deregister(stream)?,
-                RawStream::Udp(stream) => poll.registry().deregister(stream)?,
-                _ => panic!("Unexpected stream type"),
-            },
-            RegType::Rereg => match self.stream.as_mut().unwrap() {
-                RawStream::Tcp(stream) => poll.registry().reregister(
-                    stream,
-                    token,
-                    Interest::READABLE | Interest::WRITABLE,
-                )?,
-                RawStream::Udp(stream) => poll.registry().reregister(
-                    stream,
-                    token,
-                    Interest::READABLE | Interest::WRITABLE,
-                )?,
-                _ => panic!("Unexpected stream type"),
-            },
+        if let Some(s) = self.stream.as_mut() {
+            match register {
+                RegType::Reg => match s {
+                    RawStream::Tcp(stream) => poll.registry().register(
+                        stream,
+                        token,
+                        Interest::READABLE | Interest::WRITABLE,
+                    )?,
+                    RawStream::Udp(stream) => poll.registry().register(
+                        stream,
+                        token,
+                        Interest::READABLE | Interest::WRITABLE,
+                    )?,
+                    _ => panic!("Unexpected stream type"),
+                },
+                RegType::Dereg => match s {
+                    RawStream::Tcp(stream) => poll.registry().deregister(stream)?,
+                    RawStream::Udp(stream) => poll.registry().deregister(stream)?,
+                    _ => panic!("Unexpected stream type"),
+                },
+                RegType::Rereg => match s {
+                    RawStream::Tcp(stream) => poll.registry().reregister(
+                        stream,
+                        token,
+                        Interest::READABLE | Interest::WRITABLE,
+                    )?,
+                    RawStream::Udp(stream) => poll.registry().reregister(
+                        stream,
+                        token,
+                        Interest::READABLE | Interest::WRITABLE,
+                    )?,
+                    _ => panic!("Unexpected stream type"),
+                },
+            }
+        } else {
+            return Err(NxtError {
+                code: CONNECTION,
+                detail: "stream not found".to_string(),
+            });
         }
         Ok(())
     }
